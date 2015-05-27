dc51b9458d7c91dbca2fd717d78f0a9febf699be
Ilari Aarnio <jia@iki.fi>
2015-05-27 22:10:23 +0300

    cosmetic

--- src/main/java/fi/codesys/movie/rest/JaxRsActivator.java
+++ src/main/java/fi/codesys/movie/rest/JaxRsActivator.java
@@ -3,9 +3,7 @@ package fi.codesys.movie.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * Resources are served relative to the servlet path specified in the {@link ApplicationPath} annotation.
- */
+// The API root path 
 @ApplicationPath("/api")
 public class JaxRsActivator extends Application {
     /* class body intentionally left blank */
